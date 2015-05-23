86c1c5ce2da6e9416b3de763c9d6f77a6fac38e5
blowfishpro <blowfishpro@yahoo.com>
2015-05-22 18:54:51 -0700

    KSP 1.0.x compatibility

--- Modules/AnimateValue/KModuleAnimateStaticPressure.cs
+++ Modules/AnimateValue/KModuleAnimateStaticPressure.cs
@@ -16,18 +16,18 @@
 /* * * * * * * * * * * * * * * */
 
 /// <summary>
-/// Animated based on: 'this.vessel.staticPressure'.
+/// Animated based on: 'this.vessel.staticPressurekPa'.
 /// Value is the static pressure of the atomosphere
 /// at the current position of this part's vessel.
 /// </summary>
 [KPartModuleConfigurationDocumentation(
-"\n//Animated based on: 'this.vessel.staticPressure'." +
+"\n//Animated based on: 'this.vessel.staticPressurekPa'." +
 "\n//Value is the static pressure of the atomosphere" +
 "\n//at the current position of this part's vessel.")]
 public class KModuleAnimateStaticPressure : KModuleAnimateValue
 {
     protected override float SolveNormalTime()
     {
-        return (float)(this.vessel.staticPressure - MinValue) / _Denominator;
+        return (float)(this.vessel.staticPressurekPa - MinValue) / _Denominator;
     }
 }
